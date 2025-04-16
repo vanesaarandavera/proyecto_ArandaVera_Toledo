a0d2997b22d3e7e5ce0cb3a669133185265659cb
AgusT <agustina1toledo1@gmail.com>
2025-04-15 21:43:42 -0300

    Se agrego los generos en el menu

--- app/Views/front/nav_view.php
+++ app/Views/front/nav_view.php
@@ -31,13 +31,14 @@
       </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Género
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Ciencia Ficción</a></li>
+            <li><a class="dropdown-item" href="#">Clásicos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Romance</a></li>
+            <li><a class="dropdown-item" href="#">Thriller y suspenso</a></li>
           </ul>
         </li>
         <li class="nav-item">
