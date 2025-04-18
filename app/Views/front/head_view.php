9adee8dbbe0a739725d317ea3960d9f4f5f016bc
AgusT <agustina1toledo1@gmail.com>
2025-04-17 21:45:58 -0300

    Estilo al nav_view.php

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -7,6 +7,7 @@
     <title><?php echo ($titulo); ?></title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="" crossorigin="">
     <link rel="stylesheet" href="assets/css/miestilo.css">
+    <link rel="stylesheet" href="assets/css/nav.css">
     <script src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
 
 </head>
