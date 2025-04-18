59692e6d38d4187464a75782f64337f1a275b404
AgusT <agustina1toledo1@gmail.com>
2025-04-18 11:39:46 -0300

    se modifico la imagen 3 del carousel

--- app/Views/front/inicio.php
+++ app/Views/front/inicio.php
@@ -13,7 +13,7 @@
         <img src="assets\img\imgCarr2.webp" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="assets\img\imgCarr3.webp" class="d-block w-100" alt="...">
+        <img src="assets\img\imgCarr3.png" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
