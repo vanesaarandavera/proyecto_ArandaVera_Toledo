197992eb18b44b94bc5b72f83e09f6d4ac0915fb
AgusT <agustina1toledo1@gmail.com>
2025-04-19 15:11:46 -0300

    Se modifico el archivo quienes-comos.php

--- app/Views/front/quienes_somos.php
+++ app/Views/front/quienes_somos.php
@@ -1,31 +1,48 @@
-<div>
-    <div>
-        <h3>Quienes Somos</h3>
-        <p>Somos amigas unidas por los libros, convencidas de que cada historia tiene el poder de inspirar, transformar y conectar a las personas. <br>
+<div class="centrado quienes-somos" >
+    <div class="quienes-somos-section" >
+        <h3 class="titulo" >Quienes Somos</h3>
+        <p class="subtitulo" >Voces de papel</p>
+        
+        <p class="quienes-somos-introduccion">Somos amigas unidas por los libros, convencidas de que cada historia tiene el poder de inspirar, transformar y conectar a las personas. <br>
         Nuestra misión es ofrecer una cuidadosa selección de libros que enriquezcan la mente y el alma, desde los clásicos imprescindibles hasta las novedades más fascinantes. Nos esforzamos por crear un espacio donde los lectores de todas las edades y gustos puedan encontrar su próxima gran aventura literaria.       <br> 
         Más que una tienda de libros, somos una comunidad de amantes de la lectura que buscan compartir su entusiasmo y contagiar el placer de sumergirse en nuevas historias. Porque detrás de cada libro hay un mundo por descubrir, y nosotras queremos ayudarte a encontrar el tuyo.
         </p>
-        <p>Voces de papel</p>
     </div>
-    <div>
-        <h3>Nuestro equipo</h3>
-        <div>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+    <div class="quienes-somos-section">
+        <h3 class="titulo" >Nuestro equipo</h3>
+        <div class="quienes-somos-equipo" >
+            <div class="card quienes-somos-equipo-card" >
+                <div class="quienes-somos-equipo-card-img" >
+                    <img src="assets\img\perfil-girasol.jpg" class="card-img-top " alt="perfil de agustina">
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <h5 class="card-title quienes-somos-card-titulo">Agustina</h5>
+                    <p class="card-text quienes-somos-card-texto">Servicio al cliente.</p>
+                    <hr class="quienes-somos-hr">
+                    <a href="https://web.whatsapp.com/" class="card-link quienes-somos-card-texto">Whatssap</a>
+                    <p class="card-text quienes-somos-card-texto">vocesdepapel@gmail.com</p>
                 </div>
+                
+            </div>
+            <div class="card quienes-somos-equipo-card" >
+                <div class="quienes-somos-equipo-card-img">
+                    <img src="assets\img\perfil-lis.jpg" class="card-img-top " alt="perfil-de-vanesa">
+                </div>
+                
+                <div class="card-body">
+                    <h5 class="card-title quienes-somos-card-titulo">Vanesa</h5>
+                    <p class="card-text quienes-somos-card-texto">Servicio al cliente.</p>
+                    <hr class="quienes-somos-hr">
+                    <a href="https://web.whatsapp.com/" class="card-link quienes-somos-card-texto">Whatssap</a>
+                    <p class="card-text quienes-somos-card-texto">vocesdepapel@gmail.com</p>
+                </div>
+                
             </div>
         </div>
+    </div>
+    <div class="quienes-somos-section">
+        <h3 class="titulo">Donde nos podés encontrar</h3>
+        <iframe class="quienes-somos-mapa"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d226565.6282836834!2d-59.13918050546876!3d-27.466521399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456ca6a4295499%3A0x85d9453ae068a21b!2sCentro%20Cultural%20Universitario!5e0!3m2!1sen!2sar!4v1745020501227!5m2!1sen!2sar"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
