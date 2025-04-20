362a5d883197de0ce51536ea0d034bb5d2a0fa8f
AgusT <agustina1toledo1@gmail.com>
2025-04-20 16:32:53 -0300

    Se creo el archivo comerializacion.css

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="assets/css/quienes-somos.css">
     <link rel="stylesheet" href="assets/css/terminoYUsos.css">
     <link rel="stylesheet" href="assets/css/contacto.css">
+    <link rel="stylesheet" href="assets/css/comercializacion.css">
     <script src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
 
 </head>
