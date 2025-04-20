444b9de927f2ab93b586791b62693599484661f0
AgusT <agustina1toledo1@gmail.com>
2025-04-20 16:34:28 -0300

    Se modifico el archivo comercializacion.php

--- app/Views/front/comercializacion.php
+++ app/Views/front/comercializacion.php
@@ -1,3 +1,61 @@
-<div>
-    <h1>    Comercializacion</h1>
+<div class="centrado comercializacion-section" >
+    <h1 class="titulo" >    Comercializacion</h1>
+    <h4 class="subtitulo" >Envio gratis a partir de 10000 pesos</h4>
+    <div class="accordion comercializacion-acordeon" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button comercializacion-acordeon-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Tipos de entrega.
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p class="comercializacion-acordeon-p" ><strong>Hacemos envíos a todo el país.</strong> con Correo Argentino. Contamos con Envíos Express a domicilio en AMBA, y retiro en sucursales Correo Argentino. También contamos con retiro sin cargo en todas nuestras sucursales del país. Al iniciar la compra en base al código postal que ingreses, se desplegarán todas las opciones de entrega disponibles y incluyendo nuestras sucursales.</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button  comercializacion-acordeon-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+      Forma de envio
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p class="comercializacion-acordeon-p"><strong>Envío a domicilio</strong> Tenemos dos opción de envío a domicilio disponibles. Envío estándar con Andreani a todo el país (excepto Tierra del Fuego) y envío express en Capital Federal y Gran Buenos Aires con RapiBoy. El costo del envío y el tiempo estimado de entrega podés calcularlo en la página del producto ingresando tu código postal.</p>
+        <p class="comercializacion-acordeon-p"><strong>Envío Express</strong>
+        Tenemos miles de productos con envío rápido exclusivo para CABA y GBA. Tus compras realizadas de lunes a vienes antes de las 12:00 del mediodía son entregadas en el día (no incluye feriados), y luego de ese horario se entregan al día siguiente (solo días hábiles). La franja horaria de entrega para las compras despachadas con esta modalidad es entre las 9:00 y las 21:00 hs. El servicio lo realizamos con la empresa Rapiboy. En el caso de que no te encuentres en tu domicilio, realizaremos una segunda visita al día siguiente. (solo días hábiles). </p>
+        <p class="comercializacion-acordeon-p"><strong>Envío Estándar</strong> Todos nuestros productos cuentan con envío a domicilio a toda la Argentina (excepto Tierra del Fuego) a través del correo Andreani. La franja horaria de entrega para este servicio es de lunes a viernes de 9:00 a 18:00 hs.
+
+        En el caso de que no te encuentres en tu domicilio, te dejarán un aviso y se realizará una segunda visita dentro de las siguientes 48 hs. Finalmente, si no se pudiera concretar la entrega, se dejará tu paquete en custodia de la sucursal Andreani más cercana para que puedas retirarlo durante 7 días hábiles presentándote con tu DNI y número de envío.</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed comercializacion-acordeon-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Pagos: 3 y 6 cuotas sin interes con tarjetas bancarizadas.
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <div class="comercializacion-acordeon-pagos">
+            <img  src="assets\img\Logo-Naranja-X.png" alt="logo-naranjax">
+            <p class="comercializacion-acordeon-p-bancos"><strong>4 cuotas sin interes. </strong>Con Tarjeta NaranjaX en TODA tu compra</p>
+        </div>
+        <hr class="comercilaizacion-hr">
+        <div class="comercializacion-acordeon-pagos">
+            <img  src="assets\img\Banco_Nación.svg.png" alt="">
+            <p class="comercializacion-acordeon-p-bancos"><strong>30% de descuento</strong> Pagando con QR MODO y Crédito Visa o Mastercard. Tope de reintegro: $12.000 por cliente y por semana.</p>
+        </div>
+        <hr class="comercilaizacion-hr">
+        <div class="comercializacion-acordeon-pagos">
+            <img   src="assets\img\Logo_Banco_Macro.svg.png" alt="">
+            <p class="comercializacion-acordeon-p-bancos"><strong>20% de descuento</strong>Pagando con QR MODO y Crédito Visa o Mastercard. Tope de reintegro: $15.000 por cliente y por semana. </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+</div>
+
