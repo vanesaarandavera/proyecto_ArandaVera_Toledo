541b0d2d71244efd481578b3bc196a500531663b
AgusT <agustina1toledo1@gmail.com>
2025-04-23 16:44:51 -0300

    modificacion de inicio.php

--- app/Views/front/inicio.php
+++ app/Views/front/inicio.php
@@ -25,12 +25,44 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
-  <div class="card" style="width: 18rem;">
-  <img src="assets\img\portada-libro-de-los-cuentos-perdidos-j-r-r-tolkien.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Hª Tierra Media Nº 01/12 El libro de los cuentos perdidos 1</h5>
-    <p class="card-text">J. R. R. Tolkien</p>
-    <p class="card-text">Precio   $31700.00</p>
-    <a href="#" class="btn btn-primary">Comprar</a>
-  </div>
+  <div class="inicio-libros  centrado">
+    <h3>Todos los libros</h3>
+    <div class="inicio-libros-tarjetas inicio-flex">
+      <div class="card  inicio-libros-tarjetas-item" >
+        <img src="assets\img\portada-libro-de-los-cuentos-perdidos-j-r-r-tolkien.jpg" class="card-img-top" alt="...">
+        <div class="card-body inicio-libros-tarjetas-item-texto">
+          <h5 class=" inicio-libros-tarjetas-item-texto-titulo">Hª Tierra Media Nº 01/12 El libro de los cuentos perdidos 1</h5>
+          <p class=" inicio-libros-tarjetas-item-texto-titulo-p">J. R. R. Tolkien</p>
+          <p class=" inicio-libros-tarjetas-item-texto-titulo-precio">Precio $31700.00</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>
+      <div class="card inicio-libros-tarjetas-item" >
+        <img src="assets\img\portada_ese-algo-especial_sandra-miro.jpg" class="card-img-top" alt="...">
+        <div class="card-body inicio-libros-tarjetas-item-texto">
+          <h5 class="inicio-libros-tarjetas-item-texto-titulo">Ese algo especial</h5>
+          <p class="card-text">Sandra Miro</p>
+          <p class="card-text">Precio $25000.00</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>
+      <div class="card inicio-libros-tarjetas-item">
+        <img src="assets\img\portada_la-lengua-de-los-elfos_luis-gonzalez-b.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">La lengua de los elfos</h5>
+          <p class="card-text">Luis Gonzalez</p>
+          <p class="card-text">Precio $35000.00</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>
+      <div class="card inicio-libros-tarjetas-item" >
+        <img src="assets\img\portada_shanghai-inmortal_ay-chao_.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Shanghai inmortal</h5>
+          <p class="card-text">Ay Chao</p>
+          <p class="card-text">Precio $40000.00</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>
+    </div>
   </div>
