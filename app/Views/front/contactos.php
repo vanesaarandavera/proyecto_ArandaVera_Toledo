53ef3920cd4e73400ed308e66507385bf070caad
AgusT <agustina1toledo1@gmail.com>
2025-04-27 15:57:01 -0300

    Se modifico el tamaño de las letras en contactos.php

--- app/Views/front/contactos.php
+++ app/Views/front/contactos.php
@@ -2,19 +2,19 @@
 <div class="container">
     <div class="containerContacto">
         <h1>Datos de Contacto</h1>
-        <h3><strong>Nombre de la Empresa: </strong> Voces de Papel</h3>
-        <h3><strong>Dirección:</strong> Calle 9 de Julio 1272, Corrientes, Argentina</h3>
-        <h3><strong>Teléfono:</strong> +54 379 4989898</h3>
-        <h><strong>Email:</strong> info@vocesdepapel.com</h>
+        <h3 class="contactos-dato" ><strong>Nombre de la Empresa: </strong> Voces de Papel</h3>
+        <h3 class="contactos-dato"><strong>Dirección:</strong> Calle 9 de Julio 1272, Corrientes, Argentina</h3>
+        <h3 class="contactos-dato"><strong>Teléfono:</strong> +54 379 4989898</h3>
+        <h3 class="contactos-dato"><strong>Email:</strong> info@vocesdepapel.com</h3>
     </div>
     <div class="formularioContacto">
         <h2>Comunicate con nosotros</h2>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <label for="exampleFormControlInput1" class="form-label cotacto-form-etiquetas">Email address</label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+            <label for="exampleFormControlTextarea1" class="form-label cotacto-form-etiquetas">Mensaje</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
