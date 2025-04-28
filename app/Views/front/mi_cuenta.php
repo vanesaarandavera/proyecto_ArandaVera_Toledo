c1a1e1af2352e55ac6217eb2148b8695d101a66f
vanesaarandavera <vanesaa715@gmail.com>
2025-04-28 14:00:58 -0300

    "cambios de fuente en mi cuenta"

--- app/Views/front/mi_cuenta.php
+++ app/Views/front/mi_cuenta.php
@@ -1,19 +1,19 @@
 <body>
     <div class="container mt-5 mb-5 d-flex justify-content-center align-items-center vh-100">
         <div class="col-md-6 col-lg-4 p-4 rounded shadow bg-light">
-            <h2 class="text-center mb-4">Ingresa a tu cuenta</h2>
+            <h2 class="titulo text-center mb-4">Ingresa a tu cuenta</h2>
             <form action="/action_page.php" class="was-validated">
                 <div class="mb-3">
                     <label for="uname" class="form-label">Usuario:</label>
                     <input type="text" class="form-control" id="uname" placeholder="Ingresa nombre de usuario" name="uname" required>
                     <div class="valid-feedback">Valido.</div>
-                    <div class="invalid-feedback">Por favor rellena este campo.</div>
+                    <div class="invalid-feedback fs-4">Por favor rellena este campo.</div>
                 </div>
                 <div class="mb-3">
                     <label for="pwd" class="form-label">Contraseña:</label>
                     <input type="password" class="form-control" id="pwd" placeholder="Ingresa contraseña" name="pswd" required>
                     <div class="valid-feedback">Valido.</div>
-                    <div class="invalid-feedback">Por favor rellena este campo.</div>
+                    <div class="invalid-feedback fs-4">Por favor rellena este campo.</div>
                 </div>
                 <div class="d-grid">
                     <button type="submit" class="btn btn-primary">Continuar</button>
