894c10ef954b00dd78c170d8600f6283b9b02cda
vanesaarandavera <vanesaa715@gmail.com>
2025-04-28 14:45:03 -0300

    "algunos comentarios"

--- app/Views/front/contactos.php
+++ app/Views/front/contactos.php
@@ -1,5 +1,6 @@
 <body>
 <div class="container">
+<!-- Datos de la empresa-->
     <div class="containerContacto">
         <h1>Datos de Contacto</h1>
         <h3 class="contactos-dato" ><strong>Nombre de la Empresa: </strong> Voces de Papel</h3>
@@ -7,6 +8,7 @@
         <h3 class="contactos-dato"><strong>Teléfono:</strong> +54 379 4989898</h3>
         <h3 class="contactos-dato"><strong>Email:</strong> info@vocesdepapel.com</h3>
     </div>
+    <!--inicio de la logica del formulario-->
     <div class="formularioContacto">
         <h2>Comunicate con nosotros</h2>
         <div class="mb-3">
