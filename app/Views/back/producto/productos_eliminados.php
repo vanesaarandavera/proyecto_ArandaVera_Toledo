c341b50d44bcc16d77be0af6c901f14ca865e3a9
AgusT <agustina1toledo1@gmail.com>
2025-06-17 22:11:55 -0300

    Se creo la vista de los productos eliminados para el CRUD de productos, productos_eliminados.php

---
+++ app/Views/back/producto/productos_eliminados.php
@@ -0,0 +1,69 @@
+<div>
+    <?php
+    if (session()->getFlashdata('success')) : ?>
+        <div class="alert alert-success alert-dismissible">
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            <?php echo session()->getFlashdata('success')  ?>
+        </div>
+    <?php elseif (session()->getFlashdata('failed')) : ?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            <?php echo session()->getFlashdata('failed') ?>
+        </div>
+    <?php endif; ?>
+</div>
+<div class="container mt-5">
+    <div class="d-flex justify-content-end">
+        <a href="<?php echo site_url('listaProductos') ?>" class="btn btn-success btn-sm mt-1">Volver</a>
+    </div>
+    <div class="mt-3">
+        <div class=" table-responsive">
+            <table class="table table-succes table-striped" id="user-list">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Titulo</th>
+                        <th>Autor</th>
+                        <th>Descripción</th>
+                        <th>Precio</th>
+                        <th>Precio de Venta</th>
+                        <th>Stock</th>
+                        <th>Imagen</th>
+                        <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($producto) : ?>
+                        <?php foreach ($producto as $unproducto): ?>
+                            <?php $eliminado = $unproducto['eliminado']; ?>
+                            <?php if ($eliminado == '1'): ?>
+                                <tr>
+                                    <td><?= esc($unproducto['id_producto']) ?></td>
+                                    <td><?= esc($unproducto['nombre_prod']) ?></td>
+                                    <td><?= esc($unproducto['autor']) ?></td>
+                                    <td><?= esc($unproducto['descripcion']) ?></td>
+                                    <td>$<?= number_format($unproducto['precio'], 2) ?></td>
+                                    <td>$<?= number_format($unproducto['precio_vta'], 2) ?></td>
+                                    <td><?= esc($unproducto['stock']) ?></td>
+                                    <td>
+                                        <?php if (!empty($unproducto['imagen']) && file_exists(FCPATH . 'assets/uploads/' . $unproducto['imagen'])): ?>
+                                            <img src="<?= base_url('assets/uploads/' . $unproducto['imagen']) ?>" alt="Imagen <?= esc($unproducto['nombre_prod']) ?>" width="100" class="img-thumb" />
+
+                                        <?php else: ?>
+                                            <span>No hay imagen</span>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td>
+                                        <a href="<?php echo base_url('activar_pro/' . $unproducto['id_producto']) ?>" class="btn-action">Activar</a>
+                                    </td>
+                                </tr>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
