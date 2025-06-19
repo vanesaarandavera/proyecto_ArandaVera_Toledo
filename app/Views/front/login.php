ddc4acbc8151c2811c841c051e0a96e424385995
AgusT <agustina1toledo1@gmail.com>
2025-06-18 23:50:00 -0300

    Se modifico los estilos de los botones del login en miestilos.css

--- app/Views/front/login.php
+++ app/Views/front/login.php
@@ -1,19 +1,19 @@
 <div class="container mt-5 mb-5 d-flex justify-content-center align-items-center ">
     <div class="col-md-6 col-lg-4 p-4 rounded shadow bg-light">
         <h2 class="titulo text-center mb-4">Ingresa a tu cuenta</h2>
-        
-         <?php
-            if (session()->getFlashdata('success')) : ?>
-                <div class="alert alert-success alert-dismissible">
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                    <?php echo session()->getFlashdata('success')  ?>
-                </div>
-            <?php elseif (session()->getFlashdata('failed')) : ?>
-                <div class="alert alert-danger alert-dismissible">
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                    <?php echo session()->getFlashdata('failed') ?>
-                </div>
-            <?php endif; ?>
+
+        <?php
+        if (session()->getFlashdata('success')) : ?>
+            <div class="alert alert-success alert-dismissible">
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                <?php echo session()->getFlashdata('success')  ?>
+            </div>
+        <?php elseif (session()->getFlashdata('failed')) : ?>
+            <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                <?php echo session()->getFlashdata('failed') ?>
+            </div>
+        <?php endif; ?>
 
         <form action="<?= base_url('enviarlogin') ?>" class="was-validated" method="post">
             <div class="mb-3">
@@ -28,12 +28,16 @@
                 <div class="valid-feedback">Válido.</div>
                 <div class="invalid-feedback fs-4">Por favor, rellena este campo.</div>
             </div>
-            <div class="d-grid">
-                <button type="submit" class="btn btn-primary">Continuar</button>
+            <div class="inicio-flex login-btn" >
+                <div class="d-grid">
+                    <button type="button" class="btn btn-danger btn-cancelar" onclick="window.location.href='<?= base_url('/') ?>'">Cancelar</button>
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary btn-continuar">Continuar</button>
+                </div>
             </div>
-            <div class="d-grid">
-                <button type="button" class="btn btn-danger" onclick="window.location.href='<?= base_url('ruta/a/cancelar') ?>'">Cancelar</button>
-            </div>
+
+
         </form>
         <div class="mt-3 text-center">
             <h5 class="mb-1">
